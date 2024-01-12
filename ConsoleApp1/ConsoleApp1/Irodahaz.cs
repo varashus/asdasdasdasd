3d2b1222dd67b2e13b3d7d0c78a921add5831aee
varashus <115972590+varashus@users.noreply.github.com>
2024-01-12 11:20:30 +0100

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Irodahaz.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+
+    class Iroda
+    {
+        public string kód { get; set; }
+        public string kezdet { get; set; }
+        public List<int> irodak { get; set; }
+        public Iroda(string sor)
+        {
+            irodak = new List<int>();
+            var tmp = sor.Split(" ");
+                kód = tmp[0];
+            kezdet = tmp[1];
+            for (int i = 2; i < tmp.Length; i++)
+            {
+
+
+                irodak.Add(int.Parse(tmp[i]));
+
+
+            }
+
+        }
+        public override string ToString()
+        {
+            string returns = $"kód: {kód}, kezdet: {kezdet}, irodahaz: ";
+            foreach (var j in irodak) returns += j + " ";
+            return returns;
+        }
+
+    }
+
+
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using ConsoleApp1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Iroda> lista = new();
+        List<int> lista2Even = new();
+        List<int> lista2Odd = new();
+
+        using (var sr = new StreamReader(@"..\..\..\src\irodahaz.txt"))
+        {
+            var numbers = sr.ReadLine().Split(" ").Skip(2).Select(int.Parse).ToList();
+            lista2Even = numbers.Where(n => n % 2 == 0).ToList();
+            lista2Odd = numbers.Where(n => n % 2 != 0).ToList();
+
+            sr.ReadLine(); // Skip a line
+            while (!sr.EndOfStream)
+            {
+                lista.Add(new Iroda(sr.ReadLine()));
+                sr.ReadLine(); // Skip a line
+            }
+        }
+
+        
+
+        Console.WriteLine("\n\nIroda objects in lista with associated numbers:");
+        foreach (Iroda da in lista)
+        {
+            Console.Write($"{da.kód}, {da.kezdet}: ");
+            foreach (var number in da.irodak)
+            {
+                Console.Write(number + " ");
+            }
+            Console.WriteLine(); // Move to the next line for the next Iroda object
+        }
+    }
+}
